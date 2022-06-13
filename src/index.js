8dbed420a2e42a4aa11af88aefccc7d2fba5f053
sandeep1995 <i.am.sandeep.acharya@gmail.com>
2022-06-13 21:42:28 +0530

    no restriction

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
 const path = require('path');
 const createAllDeltas = require('./create-all-deltas');
 
-function checkIsValidConfiguarion(context, logger) {
-  if (!context.configuration.nsis.oneClick) {
-    logger.error(
-      'DeltaBuilder is currently only supported for one-click installers',
-    );
-    return false;
-  }
-
-  if (context.configuration.nsis.perMachine) {
-    logger.error('perMachine must be false for delta builds');
-    return false;
-  }
-
-  return true;
-}
-
 const DeltaBuilder = {
   build: async ({ context, options }) => {
     const { outDir } = context;
@@ -34,10 +18,6 @@ const DeltaBuilder = {
     const { productName } = options;
     const processName = options.processName || productName;
     const latestVersion = options.latestVersion || process.env.npm_package_version;
-
-    if (!checkIsValidConfiguarion(context, logger)) {
-      return;
-    }
 
     const deltaInstallerFiles = await createAllDeltas({
       outDir,
